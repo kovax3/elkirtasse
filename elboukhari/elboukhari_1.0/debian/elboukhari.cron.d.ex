119bd9f983d633488cd8a2f3a95522936b4dc5a6
nouah <nouah.ali@gmail.com>
2015-04-22 19:03:03 +0100

    delete elboukhari/

--- elboukhari/elboukhari_1.0/debian/elboukhari.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the elboukhari package
-#
-0 4	* * *	root	[ -x /usr/bin/elboukhari_maintenance ] && /usr/bin/elboukhari_maintenance
--- elboukhari/elboukhari_1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(elboukhari):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="elboukhari" command="/usr/bin/elboukhari"
